44532db3f0902d6ad99421c8428c74bbcb0e1585
bitmmehedi <baseit.mehedi@gmail.com>
2019-05-30 16:18:38 +0600

    Exam

---
+++ Exam/LabExam/LabExam/FRAS.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace LabExam
+{
+    partial class FRAS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.soldierNoTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.soldierNameTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.scoreOneTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.scoreTwoTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.scoreThreeTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.scoreFourTextBox = new System.Windows.Forms.TextBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.outputRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.showAllButton = new System.Windows.Forms.Button();
+            this.serchTextBox = new System.Windows.Forms.TextBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Soldier No";
+            // 
+            // soldierNoTextBox
+            // 
+            this.soldierNoTextBox.Location = new System.Drawing.Point(131, 42);
+            this.soldierNoTextBox.Name = "soldierNoTextBox";
+            this.soldierNoTextBox.Size = new System.Drawing.Size(100, 20);
+            this.soldierNoTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Soldier Name";
+            // 
+            // soldierNameTextBox
+            // 
+            this.soldierNameTextBox.Location = new System.Drawing.Point(131, 68);
+            this.soldierNameTextBox.Name = "soldierNameTextBox";
+            this.soldierNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.soldierNameTextBox.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Targer 1 Score";
+            // 
+            // scoreOneTextBox
+            // 
+            this.scoreOneTextBox.Location = new System.Drawing.Point(131, 94);
+            this.scoreOneTextBox.Name = "scoreOneTextBox";
+            this.scoreOneTextBox.Size = new System.Drawing.Size(100, 20);
+            this.scoreOneTextBox.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Targer 2 Score";
+            // 
+            // scoreTwoTextBox
+            // 
+            this.scoreTwoTextBox.Location = new System.Drawing.Point(131, 120);
+            this.scoreTwoTextBox.Name = "scoreTwoTextBox";
+            this.scoreTwoTextBox.Size = new System.Drawing.Size(100, 20);
+            this.scoreTwoTextBox.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(48, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Targer 3 Score";
+            // 
+            // scoreThreeTextBox
+            // 
+            this.scoreThreeTextBox.Location = new System.Drawing.Point(131, 146);
+            this.scoreThreeTextBox.Name = "scoreThreeTextBox";
+            this.scoreThreeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.scoreThreeTextBox.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(48, 172);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Targer 4 Score";
+            // 
+            // scoreFourTextBox
+            // 
+            this.scoreFourTextBox.Location = new System.Drawing.Point(131, 172);
+            this.scoreFourTextBox.Name = "scoreFourTextBox";
+            this.scoreFourTextBox.Size = new System.Drawing.Size(100, 20);
+            this.scoreFourTextBox.TabIndex = 1;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(156, 214);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 2;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // outputRichTextBox
+            // 
+            this.outputRichTextBox.Location = new System.Drawing.Point(252, 216);
+            this.outputRichTextBox.Name = "outputRichTextBox";
+            this.outputRichTextBox.Size = new System.Drawing.Size(505, 96);
+            this.outputRichTextBox.TabIndex = 3;
+            this.outputRichTextBox.Text = "";
+            // 
+            // showAllButton
+            // 
+            this.showAllButton.Location = new System.Drawing.Point(593, 187);
+            this.showAllButton.Name = "showAllButton";
+            this.showAllButton.Size = new System.Drawing.Size(75, 23);
+            this.showAllButton.TabIndex = 4;
+            this.showAllButton.Text = "Show All";
+            this.showAllButton.UseVisualStyleBackColor = true;
+            this.showAllButton.Click += new System.EventHandler(this.showAllButton_Click);
+            // 
+            // serchTextBox
+            // 
+            this.serchTextBox.Location = new System.Drawing.Point(578, 39);
+            this.serchTextBox.Name = "serchTextBox";
+            this.serchTextBox.Size = new System.Drawing.Size(100, 20);
+            this.serchTextBox.TabIndex = 5;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(466, 36);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.TabIndex = 6;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // FRAS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.serchTextBox);
+            this.Controls.Add(this.showAllButton);
+            this.Controls.Add(this.outputRichTextBox);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.scoreFourTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.scoreThreeTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.scoreTwoTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.scoreOneTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.soldierNameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.soldierNoTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "FRAS";
+            this.Text = "Fire Range Automation System";
+            this.Load += new System.EventHandler(this.FRAS_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox soldierNoTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox soldierNameTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox scoreOneTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox scoreTwoTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox scoreThreeTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox scoreFourTextBox;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.RichTextBox outputRichTextBox;
+        private System.Windows.Forms.Button showAllButton;
+        private System.Windows.Forms.TextBox serchTextBox;
+        private System.Windows.Forms.Button SearchButton;
+    }
+}
+
---
+++ Exam/LabExam/LabExam/FRAS.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace LabExam
+{
+    
+
+    public partial class FRAS : Form
+    {
+        List<string> soldierNos = new List<string>();
+        List<string> soldierNames = new List<string>();
+        List<int> targetOneScores = new List<int>();
+        List<int> targetTwoScores = new List<int>();
+        List<int> targetThreeScores = new List<int>();
+        List<int> targetFourScores = new List<int>();
+        List<int> averageScores = new List<int>();
+        List<int> totalScores = new List<int>();
+
+        public FRAS()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string soldierNo = "";
+                string soldierName = "";
+                int targetScoreOne = 0;
+                int targetScoreTwo = 0;
+                int targetScoreThree = 0;
+                int targetScoreFour = 0;
+                if (chkSoldier(soldierNoTextBox.Text))
+                {
+                    MessageBox.Show("Soldier already exist");
+                    return;
+                }
+                soldierNo = soldierNoTextBox.Text;
+                soldierName = soldierNameTextBox.Text;
+                targetScoreOne = Convert.ToInt32(scoreOneTextBox.Text);
+                targetScoreTwo = Convert.ToInt32(scoreTwoTextBox.Text);
+                targetScoreThree = Convert.ToInt32(scoreThreeTextBox.Text);
+                targetScoreFour = Convert.ToInt32(scoreFourTextBox.Text);
+                soldierNos.Add(soldierNo);
+                soldierNames.Add(soldierName);
+                targetOneScores.Add(targetScoreOne);
+                targetTwoScores.Add(targetScoreTwo);
+                targetThreeScores.Add(targetScoreThree);
+                targetFourScores.Add(targetScoreFour);
+                averageScores.Add((targetScoreOne + targetScoreTwo + targetScoreThree + targetScoreFour) / 4);
+                totalScores.Add((targetScoreOne + targetScoreTwo + targetScoreThree + targetScoreFour));
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private bool chkSoldier(string user)
+        {
+            
+                bool isExist = false;
+                foreach (string soldierChk in soldierNos)
+                {
+                    if (soldierChk == user)
+                        isExist = true;
+                }
+                return isExist;
+                
+
+        }
+
+        private void FRAS_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void showAllButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               string soldierNo = "";
+                string soldierName = "";
+                soldierName = serchTextBox.Text;
+               soldierName = soldierNameTextBox.Text;
+                int index = 0;
+                string message = "Sl\t\tSoldier No\tSoldier Name\tAverage Score\tTotal Score\n";
+
+                foreach (string soldier in soldierNos)
+           
+                {
+                    
+                    message = message + (index + 1) + "\t\t" + soldier + "\t\t" + soldierNames[index]
+                    +"\t\t"+averageScores[index]+"\t\t"+totalScores[index]+"\n";
+                    index++;
+                }
+
+                outputRichTextBox.Text = message;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (chkSoldier(serchTextBox.Text))
+                {
+                    int index = 0;
+                    string message = "Sl\t\tSoldier No\tSoldier Name\tAverage Score\tTotal Score\n";
+
+                    foreach (string soldierNo in soldierNos)
+                    {
+                        message = message + (index + 1) + "\t\t" + soldierNo + "\t\t" + soldierNames[index] + "\t\t" + averageScores[index] + "\t\t" + totalScores[index] + "\n";
+                        index++;
+                    }
+
+                    outputRichTextBox.Text = message;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+          
+            
+        }
+    }
+}
