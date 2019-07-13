ecaea4ac0d2108a1d44590d9141e0cf0d172ced7
shafin1984 <shafin1984@yahoo.com>
2019-07-13 23:18:31 +0600

    Assignment#5

---
+++ Assignment5/LibApp/LibApp.Models/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibApp.Models.Models
+{
+    public class Student
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Roll { get; set; }
+        public string Address { get; set; }
+        public int Contact { get; set; }
+    }
+}
---
+++ Assignment5/LibApp/LibApp/StudentUi.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibApp.Models.Models;
+using LibApp.BLL.BLL;
+
+namespace LibApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        StudentManager _studentManager;
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            Student student = new Student();
+            _studentManager = new StudentManager();
+            student.ID = idTextBox.Text;
+            student.Name = nameTextBox.Text;
+            student.Roll = rollTextBox.Text;
+            student.Address = addressTextBox.Text;
+            student.Contact = Convert.ToInt32(contactTextBox.Text);
+            int added=_studentManager.AddStudent(student);
+            if (added > 0)
+            {
+                MessageBox.Show("Student Added Successfully");
+            }
+            else
+            {
+                MessageBox.Show("Student Not Added!!!");
+            }
+        }
+
+        private void displayDataGridView_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            displayDataGridView.Rows[e.RowIndex].Cells[0].Value = (e.RowIndex + 1).ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _studentManager = new StudentManager();
+            displayDataGridView.DataSource = _studentManager.Display();
+        }
+    }
+}
